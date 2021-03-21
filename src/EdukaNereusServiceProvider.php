033d89d7772c42d31f753ae9ac196fc9bc5aea22
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-03-21 23:28:14 +0100

    Update EdukaNereusServiceProvider.php

--- src/EdukaNereusServiceProvider.php
+++ src/EdukaNereusServiceProvider.php
@@ -36,6 +36,41 @@ class EdukaNereusServiceProvider extends EdukaServiceProvider
     {
         $this->loadTestRoutes();
 
+        $this->loadCourseRoutes();
+
+        $this->loadSystemRoutes();
+    }
+
+    protected function publishResources()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/overrides/' => base_path('/'),
+        ]);
+    }
+
+    protected function registerCommands(): void
+    {
+        $this->app->bind('command.eduka:install', Install::class);
+
+        $this->commands([
+            'command.eduka:install',
+        ]);
+    }
+
+    protected function loadTestRoutes()
+    {
+        /*
+         * The test routes are only loaded in case we are not in a
+         * production environment. You are free to load your own
+         * test routes on your course service provider.
+         **/
+        if (app()->environment() != 'production') {
+            $this->loadRoutesFrom(__DIR__.'/../routes/tests.php');
+        }
+    }
+
+    protected function loadCourseRoutes()
+    {
         // Do we have a course that is not deleted?
         if (Course::exists()) {
             /*
@@ -66,43 +101,15 @@ class EdukaNereusServiceProvider extends EdukaServiceProvider
                     include $routesPath;
                  });
         }
+    }
 
-        /**
-         * Load system routes. For e.g. postmark inbound emails.
-         **/
+    protected function loadSystemRoutes()
+    {
         $systemRoutesPath = __DIR__.'/../routes/system.php';
 
         Route::middleware([IpTracing::class])
              ->group(function () use ($systemRoutesPath) {
                 include $systemRoutesPath;
              });
-    }
-
-    protected function publishResources()
-    {
-        $this->publishes([
-            __DIR__.'/../resources/overrides/' => base_path('/'),
-        ]);
-    }
-
-    protected function registerCommands(): void
-    {
-        $this->app->bind('command.eduka:install', Install::class);
-
-        $this->commands([
-            'command.eduka:install',
-        ]);
-    }
-
-    protected function loadTestRoutes()
-    {
-        /*
-         * The test routes are only loaded in case we are not in a
-         * production environment. You are free to load your own
-         * test routes on your course service provider.
-         **/
-        if (app()->environment() != 'production') {
-            $this->loadRoutesFrom(__DIR__.'/../routes/tests.php');
-        }
     }
 }
