1b32757ca9334b2bf3327e8b2bb5f08e4d539840
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-07-17 08:59:28 +0200

    wip

---
+++ resources/views/components/meta-social.blade.php
@@ -0,0 +1,12 @@
+<meta name="theme-color" content="#ffffff"/>
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:title" content="Nova Advanced UI - Develop UI Components for Laravel Nova"/>
+<meta name="twitter:site" content="@brunocfalcao"/>
+<meta name="twitter:description" content="Learn how to develop UI Components for Laravel Nova"/>
+<meta name="twitter:image" content="https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg"/>
+<meta name="twitter:creator" content="@brunocfalcao"/>
+<meta property="og:url" content="https://nova-advanced-ui.com/"/>
+<meta property="og:type" content="article"/>
+<meta property="og:description" content="Documentation for the Nova Advanced UI course."/>
+<meta property="og:image" content="https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg"/>
+<meta property="og:title" content="Nova Advanced UI - Learn how to develop UI Components for Laravel Nova."/>
