16c7b5c0eeb5b4bf86abcfdb1fb4db22dc07bd3a
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-08-15 16:07:15 +0200

    Remove payment controllers

--- routes/prelaunched.php
+++ routes/prelaunched.php
@@ -1,26 +1,11 @@
 <?php
 
-use App\Http\Middleware\VerifyCsrfToken;
 use Eduka\Nereus\Http\Controllers\Prelaunched\Welcome;
 use Eduka\Nereus\Http\Controllers\NewsletterController;
-use \Eduka\Nereus\Http\Controllers\PaymentController;
-use Eduka\Nereus\Http\Controllers\PaymentRedirectController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
 
 Route::get('/', [Welcome::class, 'index'])
       ->name('welcome.default');
 
 Route::post('/subscribe-to-newsletter',[NewsletterController::class,'subscribeToNewsletter'])
-->name('subscribe.newsletter');
-
-// @todo payment // move to proper file
-Route::get('/purchase', [PaymentController::class,'redirectToCheckoutPage'])
-    ->middleware('throttle:payment')
-    ->name('purchase.view');
-
-Route::get('/redirect-callback/{nonce}', [PaymentRedirectController::class,'index'])
-    ->name('purchase.callback');
-
-Route::post('/handle-webhook', [PaymentController::class,'handleWebhook'])
-->withoutMiddleware([VerifyCsrfToken::class]);
+    ->name('subscribe.newsletter');
