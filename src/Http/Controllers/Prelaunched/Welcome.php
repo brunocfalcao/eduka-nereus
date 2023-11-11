5c40b66e4f6101fff3a39c485f7e114ba26d45a5
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-11-11 22:50:34 +0100

    Update Welcome.php

--- src/Http/Controllers/Prelaunched/Welcome.php
+++ src/Http/Controllers/Prelaunched/Welcome.php
@@ -9,8 +9,6 @@ class Welcome extends Controller
 {
     public function index()
     {
-        dd('hi there!');
-
         return view('course::prelaunched')
                ->with(['course' => Nereus::matchCourse()]);
     }
