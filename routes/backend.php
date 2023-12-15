78ab22503660a7addb2d24f84f5668a996fa6cf9
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-12-15 19:44:14 +0100

    overrides and backend routes

--- routes/backend.php
+++ routes/backend.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'backend::default');
+Route::view('/', 'backend::home')
+     ->name('eduka.dev.home');
+
+Route::view('/password/reset', 'backend::reset-password')
+     ->name('eduka.dev.reset-password');
