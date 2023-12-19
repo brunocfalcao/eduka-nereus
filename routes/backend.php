a64fd476d8b4f85b5793b09113921e7ab6427745
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-12-19 22:26:14 +0100

    route backend changes for the reset password

--- routes/backend.php
+++ routes/backend.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'backend::home')
      ->name('eduka.dev.home');
 
-Route::view('/password/reset', 'backend::reset-password')
+Route::view('/password/reset/{token}?email={email}', 'backend::reset-password')
      ->name('eduka.dev.reset-password');
