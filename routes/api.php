b1e6c9b7aca4d4237350aeb83cf7c6ed72effacf
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-01 23:35:57 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\VerifyCsrfToken;
-use Eduka\Payments\Http\Controllers\PaymentController;
 use Eduka\Payments\Http\Controllers\WebhookController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/lemonsqueezy/webhook', WebhookController::class)
     ->name('purchase.webhook')
