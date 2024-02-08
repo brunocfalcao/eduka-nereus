5bad653aa8d5fb540d19acae43ab1f423a796f96
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-08 23:17:27 +0100

    Update backend.php

--- routes/backend.php
+++ routes/backend.php
@@ -1,9 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Eduka\Nereus\Http\Controllers\Auth\ResetPasswordController;
 
-Route::get('/password/reset/{token}?email={email}', 'backend::auth.password.reset')
-    ->name('password.reset');
+/*
+Route::get(
+    'password/reset',
+    'Auth\ForgotPasswordController@showLinkRequestForm'
+)->name('password.request');
+
+Route::post(
+    'password/email',
+    'Auth\ForgotPasswordController@sendResetLinkEmail'
+)->name('password.email');
+*/
+
+Route::get(
+    'password/reset/{token}?email={email}',
+    [ResetPasswordController::class, 'showResetForm']
+)->name('password.reset');
+
+/*
+Route::post(
+    'password/reset',
+    'Auth\ResetPasswordController@reset'
+)->name('password.update');
+*/
 
 Route::get('/', function () {
     return 'hello world';
