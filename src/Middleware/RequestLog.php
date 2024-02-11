4e067fa3fc61c402eafde9c7a4b9fa07048eaa0e
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-11 22:32:18 +0100

    Update RequestLog.php

--- src/Middleware/RequestLog.php
+++ src/Middleware/RequestLog.php
@@ -6,16 +6,25 @@ use Closure;
 use Eduka\Cube\Models\EdukaRequestLog;
 use Eduka\Nereus\Facades\Nereus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
 
 class RequestLog
 {
     public function handle(Request $request, Closure $next)
     {
+        $route = Route::getRoutes()->match($request);
+
         EdukaRequestLog::create([
             'referrer' => $request->headers->get('referer'),
             'url' => $request->fullUrl(),
-            'request_payload' => $request->all(),
-            'request_headers' => $request->headers->all(),
+            'payload' => Arr::dot($request->all()),
+            'headers' => Arr::dot($request->headers->all()),
+
+            'route' => $route?->getName(),
+            'parameters' => Arr::dot($route?->parameters()),
+            'middleware' => Arr::dot($route?->middleware()),
+
             'organization_id' => Nereus::organization()?->id,
             'course_id' => Nereus::course()?->id,
         ]);
