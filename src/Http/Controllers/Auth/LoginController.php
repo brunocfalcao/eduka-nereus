a2b841fc5b229f15613f1011b51722d45343e336
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-05-19 17:31:35 +0200

    Update LoginController.php

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,9 @@
 namespace Eduka\Nereus\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Eduka\Cube\Models\Course;
+use Eduka\Cube\Models\Student;
+use Eduka\Nereus\Facades\Nereus;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
@@ -70,5 +73,51 @@ class LoginController extends Controller
         return $request->wantsJson()
             ? new JsonResponse([], 204)
             : redirect('/');
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        /**
+         * We can validate his/her credentials, but also if this student
+         * should have access to this backend, meaning in case this account
+         * exists, we need to verify if the student bought a course that
+         * belongs to this respective backend.
+         *
+         * If it's a student admin, we don't even need to check if he is
+         * attached to a course.
+         */
+        $student = Student::firstWhere('email', $request->input('email'));
+
+        // No student? Done.
+        if (! $student) {
+            return false;
+        }
+
+        // Is this student an admin student?
+        $course = Course::firstWhere('student_admin_id', $student->id);
+
+        // Does his course he admins has this backend?
+        if ($course && $course->backend->id == Nereus::backend()->id) {
+            return $this->guard()->attempt(
+                $this->credentials($request),
+                $request->boolean('remember')
+            );
+        }
+
+        // Student doesn't belong to a course from this backend?
+        if (! $student->courses->where('backend_id', Nereus::backend()->id)->exists()) {
+            return false;
+        }
+
+        // Execute a normal user attempt.
+        return $this->guard()->attempt(
+            $this->credentials($request),
+            $request->boolean('remember')
+        );
     }
 }
