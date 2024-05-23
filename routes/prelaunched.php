c582cba3f753ddca5419687c49cf5b9c0991908c
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-05-23 22:26:12 +0200

    Update prelaunched.php

--- routes/prelaunched.php
+++ routes/prelaunched.php
@@ -3,7 +3,6 @@
 use Eduka\Nereus\Http\Controllers\Prelaunched;
 use Illuminate\Support\Facades\Route;
 use Spatie\Honeypot\ProtectAgainstSpam;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [Prelaunched::class, 'welcome'])
     ->name('prelaunched.welcome');
@@ -11,23 +10,3 @@ Route::get('/', [Prelaunched::class, 'welcome'])
 Route::post('/', [Prelaunched::class, 'subscribe'])
     ->middleware(ProtectAgainstSpam::class)
     ->name('prelaunched.subscribe');
-
-// Dump routes for debugging with formatted output
-Route::get('/route/list', function () {
-    Artisan::call('route:list');
-    $output = Artisan::output();
-
-    // Format output as HTML table
-    $lines = explode("\n", trim($output));
-    $htmlOutput = '<table border="1" cellspacing="0" cellpadding="5">';
-    foreach ($lines as $line) {
-        $htmlOutput .= '<tr>';
-        foreach (explode(' ', preg_replace('/\s+/', ' ', $line)) as $cell) {
-            $htmlOutput .= '<td>' . htmlentities($cell) . '</td>';
-        }
-        $htmlOutput .= '</tr>';
-    }
-    $htmlOutput .= '</table>';
-
-    return $htmlOutput;
-})->name('route.list');
