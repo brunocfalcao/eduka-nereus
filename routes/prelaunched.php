bf375102152554befe080a5ad8bf85d2499528b4
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-05-23 22:19:34 +0200

    Update prelaunched.php

--- routes/prelaunched.php
+++ routes/prelaunched.php
@@ -3,8 +3,7 @@
 use Eduka\Nereus\Http\Controllers\Prelaunched;
 use Illuminate\Support\Facades\Route;
 use Spatie\Honeypot\ProtectAgainstSpam;
-
-dd('routes files read');
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [Prelaunched::class, 'welcome'])
     ->name('prelaunched.welcome');
@@ -12,3 +11,10 @@ Route::get('/', [Prelaunched::class, 'welcome'])
 Route::post('/', [Prelaunched::class, 'subscribe'])
     ->middleware(ProtectAgainstSpam::class)
     ->name('prelaunched.subscribe');
+
+// Dump routes for debugging
+Route::get('/route/list', function () {
+    Artisan::call('route:list');
+    $output = Artisan::output();
+    return nl2br($output);
+})->name('route.list');
