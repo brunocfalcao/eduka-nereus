e34f477cdcdba8300ace655fff48072f17644b41
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-06-03 10:04:27 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,28 @@
 <?php
 
+use Eduka\Cube\Models\Student;
+use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\VerifyCsrfToken;
 use Eduka\Payments\Http\Controllers\WebhookController;
-use Illuminate\Support\Facades\Route;
 
 Route::post('/lemonsqueezy/webhook', WebhookController::class)
     ->name('purchase.webhook')
     ->withoutMiddleware([VerifyCsrfToken::class]);
 
 Route::get('/lemonsqueezy/webhook', function () {
-    return response(null, 200);
+
+    $student = Student::all()->last();
+    $token = '12345';
+
+    return
+        dd(
+            eduka_route(
+                $student->courses->first()->backend->domain,
+                'password.reset',
+                [
+                    'token' => $token,
+                    'email' => urlencode($student->email),
+                ]
+            )
+        );
 })->withoutMiddleware([VerifyCsrfToken::class]);
