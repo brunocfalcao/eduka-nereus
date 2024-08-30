68bc29631e51dc6570355483d06a52498eef4109
Admir Zatega <admir.zatega1009@gmail.com>
2024-08-30 21:00:32 +0000

    HomeController single vs multiple courses + future feature comments

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -3,12 +3,11 @@
 namespace Eduka\Nereus\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
+use Eduka\Nereus\Facades\Nereus;
 
 class HomeController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
@@ -19,8 +18,29 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(): \Illuminate\Foundation\Application|View|Factory|Application
+    public function index()
     {
-        return view('backend::home');
+        if (Nereus::backend()->courses()->count() == 1) {
+            //dd(Nereus::backend()->courses()->first()->chapters()->with('episodes')->first()->episodes);
+            return view('backend::home-single-course', ['course' => Nereus::backend()->courses()->first()]);
+        } else if (Nereus::backend()->courses()->count() > 1) {
+            return view('backend::home-multiple-courses', ['courses' => Nereus::backend()->courses]);
+        }
+
+        // No courses in this backend, this shouldn't happen
+        dd("Error: no courses in this backend");
+
+        // if backend has multiple courses:
+        // backend::home-multiple-courses, pass the courses list (ALL courses)
+        // inside the view:
+        // for course in courses:
+        // for chapter in course->chapters:
+        // if chapter->canBeClickable():
+        // normal
+        // there are episodes the user can access (either free or already purchased)
+        // else:
+        // gray, if they click they get taken to the buy course page
+
+        // bought 1 out of 1 -> show single course
     }
 }
