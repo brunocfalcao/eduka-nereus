ad54969230ee1ba1c00972432b19b29546d8b943
Admir Zatega <admir.zatega1009@gmail.com>
2024-09-27 20:10:14 +0000

    Episode Page Progress

--- src/Http/Controllers/EpisodePageController.php
+++ src/Http/Controllers/EpisodePageController.php
@@ -22,59 +22,10 @@ class EpisodePageController extends Controller
 
     public function index(Episode $episode)
     {
-        $course = $episode->course()->first();
-        $chapter = $episode->chapter()->first();
-
-        $student = Auth::user();
-
-        // TODO: redo this as a query in Laravel
-        $chaptersQuery = $course->chapters()->with('episodes')->get();
-        $chapters = [];
-
-        // TODO: move all of these into eduka cube
-        $course->episode_count = 0;
-        $course->chapter_count = count($chaptersQuery);
-        $course->seen_episode_count = 0;
-        $course->total_duration = 0;
-
-        foreach ($chaptersQuery as $chapter) {
-            $chapters[] = $chapter;
-
-            $chapter->is_completed = true;
-            $chapter->is_active = false;
-
-            foreach ($chapter->episodes as $currentEpisode) {
-                $course->episode_count += 1;
-                $course->total_duration += $currentEpisode->duration;
-
-                $currentEpisode->is_active = ($currentEpisode->id == $episode->id);
-                $currentEpisode->is_seen = $student->isEpisodeSeen($currentEpisode);
-
-                if (!$currentEpisode->is_seen)
-                    $chapter->is_completed = false;
-
-                if ($currentEpisode->is_active) {
-                    $chapter->is_active = true;
-                    $course->seen_episode_count += 1;
-                }
-            }
-
-            $chapters[] = $chapter;
-        }
-
-        $course->completed_percent = (
-            $course->episode_count == 0 ? 100
-            : intval(100 * $course->seen_episode_count / $course->episode_count));
-
-        $course->duration_for_humans = '3h 15m';
-
-        // dd(Episode::where('id', $episode_uuid)->first());
-        // Todo: check if they have access to the course and episode
         return view('backend::episode', [
             'episode' => $episode,
-            'course' => $course,
-            'chapter' => $chapter,
-            'course_chapters' => $chapters
+            'chapter' => $episode->chapter()->first(),
+            'course' => $episode->course()->first()
         ]);
     }
 }
